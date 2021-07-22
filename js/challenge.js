89869e05f98dfa6b9fce5b53d20d21b4dd5f8d1a
miriamAtFlatiron <miriam.peskowitz@flatironschool.com>
2021-07-22 16:32:31 -0400

    fixed comments about solutions

--- js/challenge.js
+++ js/challenge.js
@@ -27,14 +27,11 @@ function plusOne(){
     counterDisplay.innerHTML = counter
 }
 
-// const count = parseInt(counterDisplay.innerText)
-
-function likeNumber(){
-  console.log(likeButton)
+function likeNumber(){ 
     // const counterNumber = counterDisplay.innerText 
    
-    //1.not adding because not integer? data type issue? Solution: 
-  //instead of creating new counter here, use one from line 10, already does parseInt 
+    //1.Problem: not adding likes. Why? perhaps it's a because not integer? data type issue? Solution: 
+  //instead of creating new counter here, use one from line 10, because it already does parseInt 
 
   //2/result:  likes does not increment with each counter, but it doesn't yet count several likes per number 
     if (likes == 0){
@@ -43,17 +40,19 @@ function likeNumber(){
         console.log("counter", counter)
         let li = document.createElement("li")
         li.innerText = `${counter} was liked ${likes} times`
-        // likesElement.appendChild(li)
+        // SIDE ISSUE: likesElement.appendChild(li) Jess: you were right, it doesn't matter whether you use ul or the .likes class, so I switched your code back 
         ul.appendChild(li)
-    } else {  //fixed this syntax, moved else up a line, and that fixed 
+    } else {  //3. fixed this syntax-- else began on the next line, I moved else up a line, and that fixed another problem, it catches incrementing likes number, as a result, the problem where it only incremented to 1 was fixed. Now, it can get to the "else" part of the conditional. 
         likes +=1
         console.log("else likes", likes)
         console.log("elsecounterNumber", counter)
-        // likesElement.lastChild.innerText = `${counter} was liked ${likes} times`
-        // li.innerText = `${counter} was liked ${likes} times`
+        // => commenting these out, see comments at #3. 
+            // likesElement.lastChild.innerText = `${counter} was liked ${likes} times`
+            // li.innerText = `${counter} was liked ${likes} times`
+        
+        //3.Problem: Now, withe the else statement working, it catches the likes number, but doesn't hold on to all the li's.
+        // Solution: this was the code, it was just replacing the li, instead of adding to it with the new number of likes: likesElement.appendChild(li) ==> change this, instead of appendChild, use .lastChild 
         ul.lastChild.innerText = `${counter} was liked ${likes} times`
-        //3.Problem: it catches the likes number, but doesn't hold on to all the li's, 
-        // 3=> this was the code, it was just replacing the li, instead of adding to it with the new number of likes: likesElement.appendChild(li) ==> change this, instead of appendChild, use .lastChild 
     }
 
 }
